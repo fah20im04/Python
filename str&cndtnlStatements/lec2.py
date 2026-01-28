dc6f50a1ba6aeb6a29c6790f762cd9a8dec6880d
fah20im04 <fa369383@gmail.com>
2026-01-28 20:08:48 +0600

    first commit

---
+++ str&cndtnlStatements/lec2.py
@@ -0,0 +1,8 @@
+str1 = "hello Maisha"
+str2 = ' This is another string \n with a new line character '
+str3 = ''' This is a multi-line
+string example '''
+
+print(str1[6:12])  # slicing strings
+print(str1.upper())  # converting to uppercase
+#" this is how apostrophy's work in strings "
