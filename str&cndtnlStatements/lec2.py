c9867f728fcb8598bdcff0c7b267f6fca29b1afe
fah20im04 <fa369383@gmail.com>
2026-01-28 20:43:37 +0600

    str functions

--- str&cndtnlStatements/lec2.py
+++ str&cndtnlStatements/lec2.py
@@ -1,8 +1,31 @@
-str1 = "hello Maisha"
-str2 = ' This is another string \n with a new line character '
-str3 = ''' This is a multi-line
-string example '''
+# str1 = "helloWorld"
 
-print(str1[6:12])  # slicing strings
-print(str1.upper())  # converting to uppercase
-#" this is how apostrophy's work in strings "
+# print(str1[6:12])  # slicing strings
+# print(str1.upper())  # converting to uppercase
+# #" this is how apostrophy's work in strings
+# print (str1[:-6])  # negative indexing
+# print (str1[-6:len(str1)])  # negative indexing with slicing
+
+a = "madam"
+if a == a[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+""""a = input("Enter string: ")  # "madam"
+mid = len(a) // 2
+
+b = a[mid:]              # "dam"
+rev_b = ''.join(reversed(b))  # "mad"
+
+first_half = a[:mid+1]   # "mad"
+
+print(rev_b, "its reversed b")
+print(first_half)
+
+if rev_b == first_half:
+    print("palindrome")
+else:
+    print("not palindrome")""""
+
---
+++ str&cndtnlStatements/stringFunc.py
@@ -0,0 +1,11 @@
+str = "i am a Man"
+print(str.endswith("Man"))  # True
+print(str.startswith("f"))   # True
+print(str.replace("a", "4"))
+print(str.split(" "))  # ['i', 'am',
+print(str.find("Man"))  # 7
+print(str.lower())  # i am a man
+print(str.capitalize())  # I am a man
+print(str.title())  # I Am A Man
+print(str.count("a"))  # 3              
+print(len(str))  # 11
